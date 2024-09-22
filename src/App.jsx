f50c45dc14b4ce30807fc663d9562f435cf09c06
Marshal10 <marshalmaleane123@gmail.com>
2024-09-22 15:34:58 +0530

    Treanslator app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import TranslatorApp from "./components/TranslatorApp";
 import TranslatorStart from "./components/TranslatorStart";
 
 function App() {
   return (
     <div className="w-full h-screen bg-gradient-to-l from-[#b6f492] to-[#338b93] flex justify-center items-center">
       <div className="w-[90%] max-w-lg h-[70vh] bg-[#2d2d2d] rounded-xl shadow-2xl shadow-gray-800 flex flex-col">
-        <TranslatorStart />
+        {/* <TranslatorStart /> */}
+        <TranslatorApp />
       </div>
     </div>
   );
---
+++ src/components/TranslatorApp.jsx
@@ -0,0 +1,26 @@
+function TranslatorApp() {
+  return (
+    <div>
+      <button>
+        <i className="fa-solid fa-xmark"></i>
+      </button>
+      <div>
+        <div>English</div>
+        <i className="fa-solid fa-arrows-rotate"></i>
+        <div>English</div>
+      </div>
+      <div>
+        <textarea></textarea>
+        <div>0/300</div>
+      </div>
+      <button>
+        <i className="fa-solid fa-chevron-down"></i>
+      </button>
+      <div>
+        <textarea></textarea>
+      </div>
+    </div>
+  );
+}
+
+export default TranslatorApp;
