b38d829de6b150e667185cf32af25fff0db4d23e
Marshal10 <marshalmaleane123@gmail.com>
2024-09-22 15:21:42 +0530

    Start app jsx

---
+++ src/components/TranslatorStart.jsx
@@ -0,0 +1,18 @@
+function TranslatorStart() {
+  return (
+    <div>
+      <div>
+        <span>Hello</span>
+        <span>Hola</span>
+        <span>こんにちは</span>
+        <span>Bonjour</span>
+      </div>
+      <div>
+        <h1>Translator App</h1>
+        <button>Start</button>
+      </div>
+    </div>
+  );
+}
+
+export default TranslatorStart;
